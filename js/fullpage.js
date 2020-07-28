5d4378b763c2785b3a91fc54d59a2d3d1efc71a9
Steven Cheng <steven@MacBook-Pro.local>
2020-07-27 21:58:59 -0700

    7/27

--- js/fullpage.js
+++ js/fullpage.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
     anchors:['home_section', 'project_section','contact_section'],
-    licenseKey: config.FULLPAGE_KEY,
+    licenseKey: 'D98D14A2-506D4E8E-AA59972E-00D38BF1',
     autoScrolling:true,
     navigation: true,
     touchSensitivity: 80

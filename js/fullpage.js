e9323aa5fef50e99d242cd85a6d577f4481beb54
Steven Cheng <steven@MacBook-Pro.local>
2020-12-05 22:12:56 +0800

    added fixes to the main pages and added new pages;

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-		//options here
     anchors:['home_section', 'project_section','contact_section'],
     licenseKey: 'D98D14A2-506D4E8E-AA59972E-00D38BF1',
     autoScrolling:true,
     navigation: true,
-    touchSensitivity: 80
+    touchSensitivity: 80,
+	animateAnchor: false,
 	});
 });

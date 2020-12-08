4ed4efb2034d5ac7692172a7895451c68e230b9e
Steven Cheng <steven@MacBook-Pro.local>
2020-12-08 22:59:40 +0800

    added some responsive breakpoints

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	$('#fullpage').fullpage({
-    anchors:['home_section', 'project_section','contact_section'],
-    licenseKey: 'D98D14A2-506D4E8E-AA59972E-00D38BF1',
-    autoScrolling:true,
-    navigation: true,
-    touchSensitivity: 80,
-	animateAnchor: false,
-	});
+	if($(window).width() > 1023){
+		$('#fullpage').fullpage({
+	    anchors:['home_section', 'project_section','contact_section'],
+	    licenseKey: 'D98D14A2-506D4E8E-AA59972E-00D38BF1',
+	    autoScrolling:true,
+	    navigation: true,
+	    touchSensitivity: 80,
+			animateAnchor: false,
+		});
+	}
 });
